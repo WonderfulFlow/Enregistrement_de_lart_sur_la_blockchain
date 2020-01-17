d925cfae1922f4cb8d64c13c1ff6969a99abae00
Halim Amady <halim.amady@gmail.com>
2020-01-17 18:14:29 +0100

    Ajout upload images.md

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
+var formidable = require('formidable'),
+    http = require('http'),
+    util = require('util'),
+    fs = require('fs-extra'),
+    path = require("path");
+
 
 function getConnection () {
    return  mysql.createConnection({
@@ -19,7 +25,7 @@ app.use(morgan('short'))
 
 app.use(express.static('./public'))
 
-app.post('/image_create', (req,res)=> {
+app.post('/upload', (req,res)=> {
     console.log("Trying to create a new user...")
     
     const address = req.body.create_address
@@ -39,8 +45,9 @@ app.post('/image_create', (req,res)=> {
         console.log("Inserted a new user with id:", results.insertId)
         res.end()
     })
+res.end()
 
-        res.end()
+        
 })
 
 app.get('/image/:id', (req,res) => {
@@ -73,7 +80,66 @@ app.get('/image', (req,res) => {
             res.json(rows)
         });
         
-        //res.end()
+        res.end()
+})
+
+http.createServer((req,res) => {
+    if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.write('received upload:\n\n');
+            res.end(util.inspect({fields: fields, files: files}));
+        });
+        
+        form.on('fileBegin', function(name, file) {
+        file.path = path.join(__dirname, '/temp/') + file.name;
+		});
+        form.on('progress', function(bytesReceived, bytesExpected) {
+        var percent_complete = (bytesReceived / bytesExpected) * 100;
+        console.log(percent_complete.toFixed(2));
+		});
+
+        form.on('end', function (fields, files) {
+            /* Temporary location of our uploaded file */
+            var temp_path = this.openedFiles[0].path;
+            /* The file name of the uploaded file */
+            var file_name = this.openedFiles[0].name;
+            /* Location where we want to copy the uploaded file */
+            var new_location = path.join(__dirname, '/upload/');
+
+            fs.copy(temp_path, new_location + file_name, function (err) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("success!");
+                    // Delete the "temp" file
+					fs.unlink(temp_path, function(err) {
+					if (err) {
+						console.error(err);
+						console.log("TROUBLE deletion temp !");
+						} else {
+						console.log("success deletion temp !");
+						}
+					});      
+                }
+            });        
+            
+        
+        });
+
+        return;
+    }
+
+    /* Display the file upload form. */
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+            '<form action="/upload" enctype="multipart/form-data" method="post">' +
+            '<input type="text" name="title"><br>' +
+            '<input type="file" name="upload" multiple="multiple"><br>' +
+            '<input type="submit" value="Upload">' +
+            '</form>'
+    );
 })
 
 app.get("/",(req, res) => {
